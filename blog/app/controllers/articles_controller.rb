bb7a0ee6f50f179030365d175268d234798f02db
Shunta Shimizu <askmis416@gmail.com>
2024-06-08 01:41:02 +0900

    記事を削除する機能を追加 DELETEのダイアログが表示されない(6.5)

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
@@ -39,7 +39,7 @@ class ArticlesController < ApplicationController
 
     redirect_to root_path
   end
-  
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
