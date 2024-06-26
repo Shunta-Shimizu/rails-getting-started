8af7d6d6aa62f19534365a77fc2cff7fca6dcb8b
Shunta Shimizu <askmis416@gmail.com>
2024-06-26 21:43:43 +0900

    Commentモデルを追加 (7.1)

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_02_120724) do
+ActiveRecord::Schema.define(version: 2024_06_26_123045) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
