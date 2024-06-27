3182fecdcd4571e014110e322306565a25c22390
Shunta Shimizu <askmis416@gmail.com>
2024-06-27 10:02:34 +0900

    Commentモデルのコントロラーを実装，コメントを追加できるようになった (7.4)

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_26_123045) do
+ActiveRecord::Schema.define(version: 2024_06_27_004017) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,8 +22,11 @@ ActiveRecord::Schema.define(version: 2024_06_26_123045) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
+    t.integer "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  add_foreign_key "comments", "articles"
 end
