15f033f7093b470e5704c9010dc5dc8e88d3ecb1
Shunta Shimizu <askmis416@gmail.com>
2024-06-27 14:48:39 +0900

    コメントを削除する機能(9.1) link_toをbutton_toに変更して実装

--- blog/app/javascript/packs/application.js
+++ blog/app/javascript/packs/application.js
@@ -7,8 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "@rails/actioncable";
+// import "@rails/actioncable";
 
 Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// Turbolinks.start()
+// ActiveStorage.start()
